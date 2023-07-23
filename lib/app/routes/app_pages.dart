41c45473168b895bfe72b4f77390b7a2c7423b84
Masum Talukder <mdmasum.shuvo.cse@gmail.com>
2023-07-23 22:20:15 +0600

    appinion

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,8 +2,7 @@ import 'package:get/get.dart';
 
 import '../modules/detail/bindings/detail_binding.dart';
 import '../modules/detail/views/detail_view.dart';
-import '../modules/forget_password/bindings/forget_password_binding.dart';
-import '../modules/forget_password/views/forget_password_view.dart';
+
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 import '../modules/index/bindings/index_binding.dart';
@@ -12,14 +11,11 @@ import '../modules/profile/bindings/profile_binding.dart';
 import '../modules/profile/views/profile_view.dart';
 import '../modules/recent/bindings/recent_binding.dart';
 import '../modules/recent/views/recent_view.dart';
-import '../modules/reset_password/bindings/reset_password_binding.dart';
-import '../modules/reset_password/views/reset_password_view.dart';
+
 import '../modules/sign_in/bindings/sign_in_binding.dart';
 import '../modules/sign_in/views/sign_in_view.dart';
 import '../modules/splash/bindings/splash_binding.dart';
 import '../modules/splash/views/splash_view.dart';
-import '../modules/verify_phone_number/bindings/verify_phone_number_binding.dart';
-import '../modules/verify_phone_number/views/verify_phone_number_view.dart';
 
 part 'app_routes.dart';
 
@@ -44,21 +40,9 @@ class AppPages {
       page: () => const SignInView(),
       binding: SignInBinding(),
     ),
-    GetPage(
-      name: _Paths.FORGET_PASSWORD,
-      page: () => const ForgetPasswordView(),
-      binding: ForgetPasswordBinding(),
-    ),
-    GetPage(
-      name: _Paths.VERIFY_PHONE_NUMBER,
-      page: () => const VerifyPhoneNumberView(),
-      binding: VerifyPhoneNumberBinding(),
-    ),
-    GetPage(
-      name: _Paths.RESET_PASSWORD,
-      page: () => const ResetPasswordView(),
-      binding: ResetPasswordBinding(),
-    ),
+
+
+
     GetPage(
       name: _Paths.PROFILE,
       page: () => const ProfileView(),
